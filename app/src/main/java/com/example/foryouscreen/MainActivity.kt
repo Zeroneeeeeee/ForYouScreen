1c354f9aff31dd1bd6bdbd2be33d1b340c8cf0c8
Zerone <doviethavk@gmail.com>
2025-02-19 17:31:08 +0700

    Update Card View

--- app/src/main/java/com/example/foryouscreen/MainActivity.kt
+++ app/src/main/java/com/example/foryouscreen/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.foryouscreen
 
+import android.health.connect.datatypes.Device
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,13 +18,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Done
@@ -40,11 +45,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -52,13 +55,16 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.compose.SampleAppTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -71,48 +77,46 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(
-                            brush = Brush.linearGradient(
-                                colors = listOf(Color.White, Color(0xFFFFD6FA)),
-                                start = Offset(2000f, 0f),
-                                end = Offset(0f, 3000f),
-                            )
-                        ),
+                        ,
                     topBar = {
-                    TopAppBar(
-                        title = {
-                            Box(
-                                modifier = Modifier.fillMaxWidth(),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Text(
-                                    text = "Now in Android",
-                                    style = MaterialTheme.typography.titleLarge,
-                                    fontWeight = FontWeight.Bold
-                                )
-                            }
-                        }, navigationIcon = {
-                            IconButton(onClick = {}) {
-                                Icon(painterResource(R.drawable.ic_search),
-                                    "",
-                                    Modifier.size(24.dp))
-                            }
-                        },actions = {
-                            IconButton(onClick = {}) {
-                                Icon(painterResource(R.drawable.ic_profile_1),
-                                    "",
-                                    Modifier.size(24.dp))
-                            }
-                        },
-                        colors = TopAppBarColors(
-                            containerColor = Color.Transparent,
-                            scrolledContainerColor = Color.Transparent,
-                            navigationIconContentColor = MaterialTheme.colorScheme.onSurface,
-                            titleContentColor = MaterialTheme.colorScheme.onSurface,
-                            actionIconContentColor = MaterialTheme.colorScheme.onSurface
+                        TopAppBar(
+                            title = {
+                                Box(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(
+                                        text = "Now in Android",
+                                        style = MaterialTheme.typography.titleLarge,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                }
+                            }, navigationIcon = {
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        painterResource(R.drawable.ic_search),
+                                        "",
+                                        Modifier.size(24.dp)
+                                    )
+                                }
+                            }, actions = {
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        painterResource(R.drawable.ic_profile_1),
+                                        "",
+                                        Modifier.size(24.dp)
+                                    )
+                                }
+                            },
+                            colors = TopAppBarColors(
+                                containerColor = Color.Transparent,
+                                scrolledContainerColor = Color.Transparent,
+                                navigationIconContentColor = MaterialTheme.colorScheme.onSurface,
+                                titleContentColor = MaterialTheme.colorScheme.onSurface,
+                                actionIconContentColor = MaterialTheme.colorScheme.onSurface
+                            )
                         )
-                    )
-                },
+                    },
                     bottomBar = {
                         BottomAppBar {
                             NavigationBarItem(
@@ -143,16 +147,17 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                     }) { innerPadding ->
-                    Box(modifier = Modifier
-                        .fillMaxSize()
-                        .background(
-                            brush = Brush.linearGradient(
-                                colors = listOf(Color.White, Color(0xFFFFD6FA)),
-                                start = Offset(2000f, 0f),
-                                end = Offset(0f, 4000f),
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(
+                                brush = Brush.linearGradient(
+                                    colors = listOf(Color(0xFFFAEEEF), Color(0xFFFFD6FA)),
+                                    start = Offset(2000f, 0f),
+                                    end = Offset(0f, 3000f),
+                                )
                             )
-                        )
-                    ){
+                    ) {
                         Body(
                             modifier = Modifier.padding(innerPadding)
                         )
@@ -175,11 +180,12 @@ fun PeopleItems(
                 .size(70.dp, 50.dp)
 
         ) {
-            Box(modifier= Modifier
-                .clip(shape = CircleShape)
-                .wrapContentSize()
-                .align(Alignment.BottomCenter)
-            ){
+            Box(
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .wrapContentSize()
+                    .align(Alignment.BottomCenter)
+            ) {
                 Image(
                     painterResource(icon),
                     "",
@@ -234,10 +240,13 @@ fun Body(modifier: Modifier = Modifier) {
     val isAnyItemSelected = topicItems.any { it.isAdded }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize().verticalScroll(
+            state = rememberScrollState()
+        )
     ) {
         Spacer(modifier = Modifier.padding(top = 24.dp))
-        Text("What are you interested in?",
+        Text(
+            "What are you interested in?",
             fontWeight = FontWeight.Bold,
             modifier = Modifier
                 .fillMaxWidth()
@@ -285,10 +294,12 @@ fun Body(modifier: Modifier = Modifier) {
                     isAdded = item.isAdded,
                     onButtonStateChange = { newState ->
                         // Cập nhật trạng thái của từng item trong danh sách
-                        val updatedItem = item.copy(isAdded = newState) // Tạo đối tượng mới với isAdded mới
+                        val updatedItem =
+                            item.copy(isAdded = newState) // Tạo đối tượng mới với isAdded mới
                         topicItems[topicItems.indexOf(item)] = updatedItem
                     }
                 )
+
             }
         }
 
@@ -304,6 +315,8 @@ fun Body(modifier: Modifier = Modifier) {
         ) {
             Text("Done")
         }
+
+        CardView()
     }
 }
 
@@ -368,45 +381,119 @@ fun TopicItems(
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.PIXEL_7_PRO)
 @Composable
-fun CardView(
-
-){
-    Column(modifier = Modifier.fillMaxWidth()) {
+fun CardView() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth() // Đảm bảo Column chiếm toàn bộ chiều rộng
+            .padding(16.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
         Image(
             painter = painterResource(R.drawable.compose_image),
-            ""
+            contentDescription = "",
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop// Đảm bảo ảnh fill hết chiều rộng
         )
-        Column {
-            Box(Modifier.fillMaxWidth()){
-                Text(
-                    "New Compose for Wear OS codelab",
-                    style = MaterialTheme.typography.headlineSmall,
-                    modifier = Modifier.padding(start = 16.dp,end = 64.dp)
-                )
 
-                IconButton(onClick = {},
-                    modifier = Modifier
-                        .padding(end = 16.dp)
-                        .size(32.dp)
-                        .background(MaterialTheme.colorScheme.primaryContainer, CircleShape)
-                        .align(Alignment.CenterEnd)
-                ) {
-                    Icon(painterResource(R.drawable.ic_bookmark),
-                        "",
-                        modifier = Modifier.size(18.dp)
-                    )
-                }
+        // Các phần tử khác trong Column (Row, Box)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, top = 12.dp, end = 16.dp)
+        ) {
+            Image(
+                painterResource(R.drawable.ic_launcher_background),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(24.dp)
+                    .clip(CircleShape)
+            )
+            Text(
+                "Author",
+                style = MaterialTheme.typography.labelSmall,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp)
+                    .align(Alignment.CenterVertically)
+            )
+        }
+
+        Box(
+            Modifier
+                .fillMaxWidth() // Đảm bảo Box chiếm toàn bộ chiều rộng
+                .padding(vertical = 12.dp, horizontal = 16.dp)
+        ) {
+            Text(
+                "New Compose for Wear OS codelab",
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(end = 48.dp)
+            )
+
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .size(40.dp)
+                    .background(MaterialTheme.colorScheme.primaryContainer, CircleShape)
+                    .clip(CircleShape)
+                    .align(Alignment.CenterEnd)
+            ) {
+                Icon(
+                    painterResource(R.drawable.ic_bookmark),
+                    contentDescription = "",
+                    modifier = Modifier.size(18.dp)
+                )
             }
         }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            Image(
+                painterResource(R.drawable.ic_dot),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(8.dp)
+                    .align(Alignment.CenterVertically)
+            )
+            Text(
+                "January 1, 2021",
+                style = MaterialTheme.typography.labelSmall,
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+            )
+            Text(
+                "developer.android.com",
+                style = MaterialTheme.typography.labelSmall,
+            )
+        }
+
+        Text(
+            "In this codelab, you can learn how Wear OS can work with Compose, what Wear OS specific composables are available, and more!",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+        )
+    }
+    LazyRow(){
+
     }
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, device = Devices.PIXEL_7_PRO)
 @Composable
 fun GreetingPreview() {
     SampleAppTheme {
-        Body()
+        Column {
+            Body()
+
+        }
     }
 }
